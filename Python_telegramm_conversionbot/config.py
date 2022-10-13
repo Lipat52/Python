ad7498cca35473b8106e0f5438767d311d33ab7b
Lipat52 <108995088+Lipat52@users.noreply.github.com>
2022-10-13 11:46:03 +0300

    Create config.py

---
+++ Python_telegramm_conversionbot/config.py
@@ -0,0 +1,18 @@
+TOKEN = '5797920364:AAHGxrPhjOUeNLZGn68uQx7wxjbCSY6E5V4'
+
+
+keys = {
+    'Bitcoin': 'BTC',
+    'Ethereum': 'ETH',
+    'Tether': 'USDT',
+    'XRP': 'XRP',
+    'BUSD': 'BUSD',
+    'Solana': 'SOL',
+    'Chainlink': 'LINK',
+    'Cosmos': 'ATOM',
+    'Cardano': 'ADA',
+    'USD': 'USD',
+    'EUR': 'EUR',
+    'RUB': 'RUB'
+}
+
